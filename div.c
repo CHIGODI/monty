e0f923a032babf6ae9de042b9b5f04dd80d96849
CHIGODI <chiegody254@gmail.com>
2023-12-14 21:16:53 +0300

    Fix: free stack after before exit

--- div.c
+++ div.c
@@ -9,7 +9,7 @@
 void div_(stack_t **stack, unsigned int line_number)
 {
         stack_t *top, *second;
-        int diff = 0;
+        int div = 0;
 
         if (*stack == NULL || (*stack)->next == NULL)
         {
@@ -23,12 +23,12 @@ void div_(stack_t **stack, unsigned int line_number)
 	{
 		fprintf(stderr, "L%u: division by zero",
 			line_number);
-		exit(EXIT_FAILURE);
 		free_stack(stack);
+		exit(EXIT_FAILURE);
 	}
         second = (*stack)->next;
 
-        diff = ((second->n) / (top->n));
+        div = ((second->n) / (top->n));
         second->n = diff;
         *stack = second;
         second->prev = NULL;
