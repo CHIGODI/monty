4f2fb189fdb109f219cb9d8117c41abc049a4a11
CHIGODI <chiegody254@gmail.com>
2023-12-14 12:39:17 +0300

    Use fgets in main file

--- monty.c
+++ monty.c
@@ -1,4 +1,5 @@
 #include "monty.h"
+#include <ctype.h>
 /**
  * main - Entry point
  * @argc: argument count
@@ -11,49 +12,49 @@
 char *arg = NULL;
 int main(int argc, char *argv[])
 {
-	FILE *fp;
+	FILE *fp = NULL;
 	char *command = NULL;
-	size_t len = 0;
-	ssize_t read;
-	char *cmd;
-	void (*function)(stack_t **, unsigned int);
-	unsigned int line_number = 0;
+	char *cmd = NULL;
 	stack_t *stack = NULL;
+	unsigned int line_number = 0;
+	void (*function)(stack_t **, unsigned int) = NULL;
 
 	if (argc != 2)
 	{
 		write(2, "USAGE: monty file\n", 18);
 		exit(EXIT_FAILURE);
 	}
-
 	fp = fopen(argv[1], "r");
 	if (fp == NULL)
 	{
 		write(2, "Error: Can't open file\n", 23);
 		exit(EXIT_FAILURE);
 	}
-
-	while ((read = getline(&command, &len, fp)) != -1)
+	command = malloc(sizeof(char) * MAX_COMMAND_LENGTH);
+	if (command == NULL)
+	{
+		fclose(fp);
+		exit(EXIT_FAILURE);
+	}
+	while (fgets(command, sizeof(command), fp) != NULL)
 	{
 		line_number++;
 		cmd = strtok(command, " \t\r\n");
 		if (cmd == NULL)
-		{
 			continue;
-		}
 		arg = strtok(NULL, " \t\r\n");
 		function = opcode_mapper(cmd);
 		if (function == NULL)
 		{
-			fprintf(stderr, "L%u: unknown instruction %s\n", line_number, cmd);
+			fprintf(stderr, "L%u: unknown instruction %s\n",
+				line_number, cmd);
 			free(command);
 			fclose(fp);
 			exit(EXIT_FAILURE);
 		}
 		function(&stack, line_number);
 	}
-
 	free(command);
 	fclose(fp);
-	return 0;
+	return (0);
 }
