01c274056d362cb26004229643c13481b2eadbd8
CHIGODI <chiegody254@gmail.com>
2023-12-15 12:53:12 +0300

    Add: free global command

--- push.c
+++ push.c
@@ -15,20 +15,21 @@ void push(stack_t **stack, unsigned int line_number)
 	if (global.arg == NULL)
 	{
 		fprintf(stderr, "L%u: usage: push integer\n", line_number);
-		return;
+		free(global.command);
+		exit(EXIT_FAILURE);
 	}
 	data = atoi(global.arg);
 	if (data == 0 && strcmp(global.arg, "0") != 0)
 	{
 		fprintf(stderr, "L%u: usage: push integer\n", line_number);
-		free_stack(stack);
+		free(global.command);
 		exit(EXIT_FAILURE);
 	}
 	new_item = malloc(sizeof(stack_t));
 	if (new_item == NULL)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
-		free_stack(stack);
+		free(global.command);
 		exit(EXIT_FAILURE);
 	}
 
